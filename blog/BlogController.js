103cb70ae809e593f4c09adb4cb55ae3d913f169
Kalyan3081 <vijaykalyan3081@gmail.com>
2025-04-05 13:06:36 +0530

    created diff posts with diff users

--- blog/BlogController.js
+++ blog/BlogController.js
@@ -1,5 +1,6 @@
-import { createBlogPost } from "./BlogServices.js"
+// Controller does the filteration of the data
 
+import { createBlogPost, fetchBlogPosts } from "./BlogServices.js"
 
 export const BlogController = async function (req, res) {
     const { description, genre = "Politics" } = req.body
@@ -8,4 +9,13 @@ export const BlogController = async function (req, res) {
     }
     await createBlogPost({ userID: req.userId, description, genre });
     res.status(201).json({ message: "Blog post created successfully" })
+}
+
+
+export async function fetchAllBlogPostsController(req, res) {
+    const serverRes = {
+        data: await fetchBlogPosts(),
+        message: " Blog posts fetched successfully"
+    };
+    res.status(200).json(serverRes);
 }
--- model/BlogsModel.js
+++ model/BlogsModel.js
@@ -1,24 +1,42 @@
+// Model it defines the database schema
+
 import mongoose, { Schema } from "mongoose";
 
-
-const BlogSchema = new Schema({
-    description: {
+const CommentSchema = new Schema({
+    text: {
         type: String,
         required: true
     },
-    timeStamp: {
+    createdAt: {
         type: Date,
         default: Date.now
     },
     userID: {
         type: String,
         required: true
+    }
+})
+
+const BlogSchema = new Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     },
     genre: {
         type: String,
         required: true,
         default: "politics"
-    }
+    },
+    comments: [CommentSchema]
 });
 
 const Blog = mongoose.model("Blog", BlogSchema);
