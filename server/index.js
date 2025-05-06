ddb2c9f54cd0f7807ab9bcd63606b62ce3602ab3
Kalyan3081 <vijaykalyan3081@gmail.com>
2025-05-07 00:46:07 +0530

    added index.js

--- server/index.js
+++ server/index.js
@@ -9,24 +9,40 @@ import GenresRouter from "./routes/GenreRoutes.js";
 import { getSentiment } from "./blog/sentiment.js";
 
 const app = express();
-
 dotenv.config();
-app.use(cors());
+
+// ✅ Define allowed origins
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://blog-server-y4md.onrender.com'
+];
+
+// ✅ Use CORS with origin check
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
+
 app.use(express.json());
+mongoDB_connection();
 
-mongoDB_connection()
-
-app.use('/auth', AuthRoutes)
-app.use('/blog', BlogRoutes)
-app.use('/comment', CommentRouter)
+app.use('/auth', AuthRoutes);
+app.use('/blog', BlogRoutes);
+app.use('/comment', CommentRouter);
 app.use("/genres", GenresRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
 
-getSentiment()
+getSentiment();
